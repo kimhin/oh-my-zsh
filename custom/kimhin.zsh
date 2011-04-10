069a52dc4671ab438cd80b6f49678589a4170b61
Kimhin Jing <kimhin@gmail.com>
2011-04-10 16:56:42 +0800

    Modified prompt in kimhin.zsh

--- custom/kimhin.zsh
+++ custom/kimhin.zsh
@@ -5,7 +5,7 @@
 # -*- sh -*- vim:set ft=sh ai et sw=4 sts=4:
 # It might be bash like, but I can't have my co-workers knowing I use zsh
 PROMPT='
-%{$fg[green]%}%n@%m:%{$fg_bold[blue]%}%2~ $(git_prompt_info)
+%{$fg[green]%}%n@%m  $(git_prompt_info)  %{$fg_bold[blue]%}%2~ 
 %{$reset_color%}%(!.#.$) '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[red]%}‹"
