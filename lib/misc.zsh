1ac03fb03bcc3aa929234a71ace3f0168a264d3f
Xin JING <jing.xin@shuimutech.com>
2011-04-29 11:42:06 +0800

    Add uibs env_var

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -14,3 +14,8 @@ export LC_CTYPE=en_US.UTF-8
 
 ## JAVA_HOME
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+
+## UIBS IP Config
+export UIBS_ESB_URL=rmi://127.0.0.1:1099
+export UIBS_HOST_IP=127.0.0.1
+export UIBS_JDBC_URL=127.0.0.1:3306
